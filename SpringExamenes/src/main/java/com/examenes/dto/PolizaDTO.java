f4fb1c534ce3fc78ad4b38c2dbae5e2d45d8ca44
luciano <luoronio@gmail.com>
2022-09-14 17:14:50 -0300

    impresion PDF

--- SpringExamenes/src/main/java/com/examenes/dto/PolizaDTO.java
+++ SpringExamenes/src/main/java/com/examenes/dto/PolizaDTO.java
@@ -2,4 +2,105 @@ package com.examenes.dto;
 
 public class PolizaDTO {
 
+	private String  idPagoFacil; 
+	private Integer endoso; //poliza.endoso 
+	private String motivoEndoso; //motivoEndoso 
+	private String nombreLocalEmis; //poliza.localidadEmision.nombre 
+	private String inicioVigencia; //poliza.inicioVigencia 
+	private String renueva;  
+	private String fechaEmision;  //poliza.fechaEmision
+	private String tomadorView;  
+	private String numeroPolizaView;  
+	private String cuitTomador;  
+	private String productorApellidoNombre;  
+	private String matricula;  
+	private Long codProd;  
+	private String vigenciaDesde;  
+	private String vigenciaHasta;  
+	
+<field name="bolsa" class="java.lang.String">
+	<fieldDescription><![CDATA[poliza.productor.bolsaView]]></fieldDescription>
+</field>
+<field name="primaTotal" class="java.lang.Double">
+	<fieldDescription><![CDATA[poliza.primaTotal]]></fieldDescription>
+</field>
+<field name="cuotaSocial" class="java.lang.Double">
+	<fieldDescription><![CDATA[poliza.cuotaSocial]]></fieldDescription>
+</field>
+<field name="financiero" class="java.lang.Double">
+	<fieldDescription><![CDATA[poliza.financiero]]></fieldDescription>
+</field>
+<field name="impuesto" class="java.lang.Double">
+	<fieldDescription><![CDATA[poliza.impuesto]]></fieldDescription>
+</field>
+<field name="iva" class="java.lang.Double">
+	<fieldDescription><![CDATA[poliza.iva]]></fieldDescription>
+</field>
+<field name="percepcionIva" class="java.lang.Double">
+	<fieldDescription><![CDATA[percepcion]]></fieldDescription>
+</field>
+<field name="premioCotizado" class="java.lang.Double">
+	<fieldDescription><![CDATA[poliza.premioCotizado]]></fieldDescription>
+</field>
+<field name="premioTotal" class="java.lang.Double">
+	<fieldDescription><![CDATA[poliza.premioTotal]]></fieldDescription>
+</field>
+<field name="porcentajeComision" class="java.lang.Double">
+	<fieldDescription><![CDATA[poliza.porcentajeComision]]></fieldDescription>
+</field>
+<field name="cuotasIzquierda" class="java.lang.String">
+	<fieldDescription><![CDATA[seccionCuotaIzq]]></fieldDescription>
+</field>
+<field name="cuotasDerecha" class="java.lang.String">
+	<fieldDescription><![CDATA[seccionCuotaDer]]></fieldDescription>
+</field>
+<field name="tieneCasco" class="java.lang.Boolean">
+	<fieldDescription><![CDATA[tieneCasco]]></fieldDescription>
+</field>
+<field name="codigoSeguimiento" class="java.lang.String">
+	<fieldDescription><![CDATA[poliza.codigoSeguimiento.codigo]]></fieldDescription>
+</field>
+<field name="nroSocio" class="java.lang.String">
+	<fieldDescription><![CDATA[poliza.socio.numeroView]]></fieldDescription>
+</field>
+<field name="esDocPersonal" class="java.lang.Boolean">
+	<fieldDescription><![CDATA[poliza.socio.persona.esDocPersonal]]></fieldDescription>
+</field>
+<field name="codCobrador" class="java.lang.String">
+	<fieldDescription><![CDATA[poliza.codigoCobrador]]></fieldDescription>
+</field>
+<field name="observacion" class="java.lang.String">
+	<fieldDescription><![CDATA[observacionView]]></fieldDescription>
+</field>
+<field name="codigoBarraFrente" class="java.lang.String">
+	<fieldDescription><![CDATA[codigoBarraFrente]]></fieldDescription>
+</field>
+<field name="pagoFacil" class="java.lang.Boolean">
+	<fieldDescription><![CDATA[pagoFacil]]></fieldDescription>
+</field>
+<field name="domicilioTomadorView" class="java.lang.String">
+	<fieldDescription><![CDATA[domicilioTomadorView]]></fieldDescription>
+</field>
+<field name="tomadorNoAsegurado" class="java.lang.Boolean">
+	<fieldDescription><![CDATA[tomadorNoAsegurado]]></fieldDescription>
+</field>
+<field name="aseguradoView" class="java.lang.String"/>
+<field name="domicilioAseguradoView" class="java.lang.String"/>
+<field name="localidadAseguradoView" class="java.lang.String">
+	<fieldDescription><![CDATA[localidadAseguradoView]]></fieldDescription>
+</field>
+<field name="labelParaDniCuitOCuilTomador" class="java.lang.String"/>
+<field name="dniCuitOCuilTomador" class="java.lang.String"/>
+<field name="simbolo" class="java.lang.String">
+	<fieldDescription><![CDATA[poliza.moneda.simbolo]]></fieldDescription>
+</field>
+<field name="sumaAseguradaNumeric" class="java.lang.Double">
+	<fieldDescription><![CDATA[sumaAseguradaTotal]]></fieldDescription>
+</field>
+<field name="codigoPagosLink" class="java.lang.String">
+	<fieldDescription><![CDATA[codigoPagosLink]]></fieldDescription>
+</field>
+<field name="tea" class="java.lang.Float">
+	<fieldDescription><![CDATA[poliza.ramo.tea]]></fieldDescription>
+</field>
 }
