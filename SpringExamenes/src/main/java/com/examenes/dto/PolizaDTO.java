ecec35fbecefabfa8d9eed5ec9da43082df7dafe
Luciano <luciano.orono@tecso.coop>
2022-09-19 09:24:04 -0300

    arreglos

--- SpringExamenes/src/main/java/com/examenes/dto/PolizaDTO.java
+++ SpringExamenes/src/main/java/com/examenes/dto/PolizaDTO.java
@@ -17,90 +17,314 @@ public class PolizaDTO {
 	private Long codProd;  
 	private String vigenciaDesde;  
 	private String vigenciaHasta;  
+	private String bolsa;  
+	private Double primaTotal;  
+	private Double cuotaSocial;  
+	private Double financiero;  
+	private Double impuesto;  
+	private Double iva;  
+	private Double percepcionIva;  
+	private Double premioCotizado;  
+	private Double premioTotal;  
+	private Double porcentajeComision;  
+	private String cuotasIzquierda;  
+	private String cuotasDerecha;  
+	private String tieneCasco;  
+	private String codigoSeguimiento;  //poliza.codigoSeguimiento.codigo
+	private String nroSocio;  //poliza.codigoSeguimiento.codigo
+	private Boolean esDocPersonal;  //poliza.codigoSeguimiento.codigo
+	private String codCobrador;  //poliza.codigoSeguimiento.codigo
+	private String observacion;  //poliza.codigoSeguimiento.codigo
+	private String codigoBarraFrente;  //poliza.codigoSeguimiento.codigo
+	private String pagoFacil;  //poliza.codigoSeguimiento.codigo
+	private String domicilioTomadorView;  //poliza.codigoSeguimiento.codigo
+	private Boolean tomadorNoAsegurado;  //poliza.codigoSeguimiento.codigo
+	private String aseguradoView;  //poliza.codigoSeguimiento.codigo
+	private String domicilioAseguradoView;  //poliza.codigoSeguimiento.codigo
+	private String localidadAseguradoView;  //poliza.codigoSeguimiento.codigo
+	private String labelParaDniCuitOCuilTomador;  //poliza.codigoSeguimiento.codigo
+	private String dniCuitOCuilTomador;  //poliza.codigoSeguimiento.codigo
+	private String simbolo;  //poliza.codigoSeguimiento.codigo
+	private Double sumaAseguradaNumeric;  //poliza.codigoSeguimiento.codigo
+	private String codigoPagosLink;  //poliza.codigoSeguimiento.codigo
+	private Double tea;  //poliza.codigoSeguimiento.codigo
+	public String getIdPagoFacil() {
+		return idPagoFacil;
+	}
+	public void setIdPagoFacil(String idPagoFacil) {
+		this.idPagoFacil = idPagoFacil;
+	}
+	public Integer getEndoso() {
+		return endoso;
+	}
+	public void setEndoso(Integer endoso) {
+		this.endoso = endoso;
+	}
+	public String getMotivoEndoso() {
+		return motivoEndoso;
+	}
+	public void setMotivoEndoso(String motivoEndoso) {
+		this.motivoEndoso = motivoEndoso;
+	}
+	public String getNombreLocalEmis() {
+		return nombreLocalEmis;
+	}
+	public void setNombreLocalEmis(String nombreLocalEmis) {
+		this.nombreLocalEmis = nombreLocalEmis;
+	}
+	public String getInicioVigencia() {
+		return inicioVigencia;
+	}
+	public void setInicioVigencia(String inicioVigencia) {
+		this.inicioVigencia = inicioVigencia;
+	}
+	public String getRenueva() {
+		return renueva;
+	}
+	public void setRenueva(String renueva) {
+		this.renueva = renueva;
+	}
+	public String getFechaEmision() {
+		return fechaEmision;
+	}
+	public void setFechaEmision(String fechaEmision) {
+		this.fechaEmision = fechaEmision;
+	}
+	public String getTomadorView() {
+		return tomadorView;
+	}
+	public void setTomadorView(String tomadorView) {
+		this.tomadorView = tomadorView;
+	}
+	public String getNumeroPolizaView() {
+		return numeroPolizaView;
+	}
+	public void setNumeroPolizaView(String numeroPolizaView) {
+		this.numeroPolizaView = numeroPolizaView;
+	}
+	public String getCuitTomador() {
+		return cuitTomador;
+	}
+	public void setCuitTomador(String cuitTomador) {
+		this.cuitTomador = cuitTomador;
+	}
+	public String getProductorApellidoNombre() {
+		return productorApellidoNombre;
+	}
+	public void setProductorApellidoNombre(String productorApellidoNombre) {
+		this.productorApellidoNombre = productorApellidoNombre;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public Long getCodProd() {
+		return codProd;
+	}
+	public void setCodProd(Long codProd) {
+		this.codProd = codProd;
+	}
+	public String getVigenciaDesde() {
+		return vigenciaDesde;
+	}
+	public void setVigenciaDesde(String vigenciaDesde) {
+		this.vigenciaDesde = vigenciaDesde;
+	}
+	public String getVigenciaHasta() {
+		return vigenciaHasta;
+	}
+	public void setVigenciaHasta(String vigenciaHasta) {
+		this.vigenciaHasta = vigenciaHasta;
+	}
+	public String getBolsa() {
+		return bolsa;
+	}
+	public void setBolsa(String bolsa) {
+		this.bolsa = bolsa;
+	}
+	public Double getPrimaTotal() {
+		return primaTotal;
+	}
+	public void setPrimaTotal(Double primaTotal) {
+		this.primaTotal = primaTotal;
+	}
+	public Double getCuotaSocial() {
+		return cuotaSocial;
+	}
+	public void setCuotaSocial(Double cuotaSocial) {
+		this.cuotaSocial = cuotaSocial;
+	}
+	public Double getFinanciero() {
+		return financiero;
+	}
+	public void setFinanciero(Double financiero) {
+		this.financiero = financiero;
+	}
+	public Double getImpuesto() {
+		return impuesto;
+	}
+	public void setImpuesto(Double impuesto) {
+		this.impuesto = impuesto;
+	}
+	public Double getIva() {
+		return iva;
+	}
+	public void setIva(Double iva) {
+		this.iva = iva;
+	}
+	public Double getPercepcionIva() {
+		return percepcionIva;
+	}
+	public void setPercepcionIva(Double percepcionIva) {
+		this.percepcionIva = percepcionIva;
+	}
+	public Double getPremioCotizado() {
+		return premioCotizado;
+	}
+	public void setPremioCotizado(Double premioCotizado) {
+		this.premioCotizado = premioCotizado;
+	}
+	public Double getPremioTotal() {
+		return premioTotal;
+	}
+	public void setPremioTotal(Double premioTotal) {
+		this.premioTotal = premioTotal;
+	}
+	public Double getPorcentajeComision() {
+		return porcentajeComision;
+	}
+	public void setPorcentajeComision(Double porcentajeComision) {
+		this.porcentajeComision = porcentajeComision;
+	}
+	public String getCuotasIzquierda() {
+		return cuotasIzquierda;
+	}
+	public void setCuotasIzquierda(String cuotasIzquierda) {
+		this.cuotasIzquierda = cuotasIzquierda;
+	}
+	public String getCuotasDerecha() {
+		return cuotasDerecha;
+	}
+	public void setCuotasDerecha(String cuotasDerecha) {
+		this.cuotasDerecha = cuotasDerecha;
+	}
+	public String getTieneCasco() {
+		return tieneCasco;
+	}
+	public void setTieneCasco(String tieneCasco) {
+		this.tieneCasco = tieneCasco;
+	}
+	public String getCodigoSeguimiento() {
+		return codigoSeguimiento;
+	}
+	public void setCodigoSeguimiento(String codigoSeguimiento) {
+		this.codigoSeguimiento = codigoSeguimiento;
+	}
+	public String getNroSocio() {
+		return nroSocio;
+	}
+	public void setNroSocio(String nroSocio) {
+		this.nroSocio = nroSocio;
+	}
+	public Boolean getEsDocPersonal() {
+		return esDocPersonal;
+	}
+	public void setEsDocPersonal(Boolean esDocPersonal) {
+		this.esDocPersonal = esDocPersonal;
+	}
+	public String getCodCobrador() {
+		return codCobrador;
+	}
+	public void setCodCobrador(String codCobrador) {
+		this.codCobrador = codCobrador;
+	}
+	public String getObservacion() {
+		return observacion;
+	}
+	public void setObservacion(String observacion) {
+		this.observacion = observacion;
+	}
+	public String getCodigoBarraFrente() {
+		return codigoBarraFrente;
+	}
+	public void setCodigoBarraFrente(String codigoBarraFrente) {
+		this.codigoBarraFrente = codigoBarraFrente;
+	}
+	public String getPagoFacil() {
+		return pagoFacil;
+	}
+	public void setPagoFacil(String pagoFacil) {
+		this.pagoFacil = pagoFacil;
+	}
+	public String getDomicilioTomadorView() {
+		return domicilioTomadorView;
+	}
+	public void setDomicilioTomadorView(String domicilioTomadorView) {
+		this.domicilioTomadorView = domicilioTomadorView;
+	}
+	public Boolean getTomadorNoAsegurado() {
+		return tomadorNoAsegurado;
+	}
+	public void setTomadorNoAsegurado(Boolean tomadorNoAsegurado) {
+		this.tomadorNoAsegurado = tomadorNoAsegurado;
+	}
+	public String getAseguradoView() {
+		return aseguradoView;
+	}
+	public void setAseguradoView(String aseguradoView) {
+		this.aseguradoView = aseguradoView;
+	}
+	public String getDomicilioAseguradoView() {
+		return domicilioAseguradoView;
+	}
+	public void setDomicilioAseguradoView(String domicilioAseguradoView) {
+		this.domicilioAseguradoView = domicilioAseguradoView;
+	}
+	public String getLocalidadAseguradoView() {
+		return localidadAseguradoView;
+	}
+	public void setLocalidadAseguradoView(String localidadAseguradoView) {
+		this.localidadAseguradoView = localidadAseguradoView;
+	}
+	public String getLabelParaDniCuitOCuilTomador() {
+		return labelParaDniCuitOCuilTomador;
+	}
+	public void setLabelParaDniCuitOCuilTomador(String labelParaDniCuitOCuilTomador) {
+		this.labelParaDniCuitOCuilTomador = labelParaDniCuitOCuilTomador;
+	}
+	public String getDniCuitOCuilTomador() {
+		return dniCuitOCuilTomador;
+	}
+	public void setDniCuitOCuilTomador(String dniCuitOCuilTomador) {
+		this.dniCuitOCuilTomador = dniCuitOCuilTomador;
+	}
+	public String getSimbolo() {
+		return simbolo;
+	}
+	public void setSimbolo(String simbolo) {
+		this.simbolo = simbolo;
+	}
+	public Double getSumaAseguradaNumeric() {
+		return sumaAseguradaNumeric;
+	}
+	public void setSumaAseguradaNumeric(Double sumaAseguradaNumeric) {
+		this.sumaAseguradaNumeric = sumaAseguradaNumeric;
+	}
+	public String getCodigoPagosLink() {
+		return codigoPagosLink;
+	}
+	public void setCodigoPagosLink(String codigoPagosLink) {
+		this.codigoPagosLink = codigoPagosLink;
+	}
+	public Double getTea() {
+		return tea;
+	}
+	public void setTea(Double tea) {
+		this.tea = tea;
+	}
+
 	
-<field name="bolsa" class="java.lang.String">
-	<fieldDescription><![CDATA[poliza.productor.bolsaView]]></fieldDescription>
-</field>
-<field name="primaTotal" class="java.lang.Double">
-	<fieldDescription><![CDATA[poliza.primaTotal]]></fieldDescription>
-</field>
-<field name="cuotaSocial" class="java.lang.Double">
-	<fieldDescription><![CDATA[poliza.cuotaSocial]]></fieldDescription>
-</field>
-<field name="financiero" class="java.lang.Double">
-	<fieldDescription><![CDATA[poliza.financiero]]></fieldDescription>
-</field>
-<field name="impuesto" class="java.lang.Double">
-	<fieldDescription><![CDATA[poliza.impuesto]]></fieldDescription>
-</field>
-<field name="iva" class="java.lang.Double">
-	<fieldDescription><![CDATA[poliza.iva]]></fieldDescription>
-</field>
-<field name="percepcionIva" class="java.lang.Double">
-	<fieldDescription><![CDATA[percepcion]]></fieldDescription>
-</field>
-<field name="premioCotizado" class="java.lang.Double">
-	<fieldDescription><![CDATA[poliza.premioCotizado]]></fieldDescription>
-</field>
-<field name="premioTotal" class="java.lang.Double">
-	<fieldDescription><![CDATA[poliza.premioTotal]]></fieldDescription>
-</field>
-<field name="porcentajeComision" class="java.lang.Double">
-	<fieldDescription><![CDATA[poliza.porcentajeComision]]></fieldDescription>
-</field>
-<field name="cuotasIzquierda" class="java.lang.String">
-	<fieldDescription><![CDATA[seccionCuotaIzq]]></fieldDescription>
-</field>
-<field name="cuotasDerecha" class="java.lang.String">
-	<fieldDescription><![CDATA[seccionCuotaDer]]></fieldDescription>
-</field>
-<field name="tieneCasco" class="java.lang.Boolean">
-	<fieldDescription><![CDATA[tieneCasco]]></fieldDescription>
-</field>
-<field name="codigoSeguimiento" class="java.lang.String">
-	<fieldDescription><![CDATA[poliza.codigoSeguimiento.codigo]]></fieldDescription>
-</field>
-<field name="nroSocio" class="java.lang.String">
-	<fieldDescription><![CDATA[poliza.socio.numeroView]]></fieldDescription>
-</field>
-<field name="esDocPersonal" class="java.lang.Boolean">
-	<fieldDescription><![CDATA[poliza.socio.persona.esDocPersonal]]></fieldDescription>
-</field>
-<field name="codCobrador" class="java.lang.String">
-	<fieldDescription><![CDATA[poliza.codigoCobrador]]></fieldDescription>
-</field>
-<field name="observacion" class="java.lang.String">
-	<fieldDescription><![CDATA[observacionView]]></fieldDescription>
-</field>
-<field name="codigoBarraFrente" class="java.lang.String">
-	<fieldDescription><![CDATA[codigoBarraFrente]]></fieldDescription>
-</field>
-<field name="pagoFacil" class="java.lang.Boolean">
-	<fieldDescription><![CDATA[pagoFacil]]></fieldDescription>
-</field>
-<field name="domicilioTomadorView" class="java.lang.String">
-	<fieldDescription><![CDATA[domicilioTomadorView]]></fieldDescription>
-</field>
-<field name="tomadorNoAsegurado" class="java.lang.Boolean">
-	<fieldDescription><![CDATA[tomadorNoAsegurado]]></fieldDescription>
-</field>
-<field name="aseguradoView" class="java.lang.String"/>
-<field name="domicilioAseguradoView" class="java.lang.String"/>
-<field name="localidadAseguradoView" class="java.lang.String">
-	<fieldDescription><![CDATA[localidadAseguradoView]]></fieldDescription>
-</field>
-<field name="labelParaDniCuitOCuilTomador" class="java.lang.String"/>
-<field name="dniCuitOCuilTomador" class="java.lang.String"/>
-<field name="simbolo" class="java.lang.String">
-	<fieldDescription><![CDATA[poliza.moneda.simbolo]]></fieldDescription>
-</field>
-<field name="sumaAseguradaNumeric" class="java.lang.Double">
-	<fieldDescription><![CDATA[sumaAseguradaTotal]]></fieldDescription>
-</field>
-<field name="codigoPagosLink" class="java.lang.String">
-	<fieldDescription><![CDATA[codigoPagosLink]]></fieldDescription>
-</field>
-<field name="tea" class="java.lang.Float">
-	<fieldDescription><![CDATA[poliza.ramo.tea]]></fieldDescription>
-</field>
+	
 }
